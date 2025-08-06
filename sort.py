85366f0e81db1f9dc5c12c5fba188e9478ee8ccd
Julian <ziminjulian@gmail.com>
2025-08-06 15:47:48 +0300

    Update sort.py

--- sort.py
+++ sort.py
@@ -3,23 +3,26 @@ def selection_sort(_lst: list) -> list:
     lst = _lst.copy()
     N = len(lst)
     for i in range(N-1):
-        m = lst[i]
-        p = i
+        m, p = lst[i], i
         for j in range(i+1, N):
             if lst[j] < m:
-                m = lst[j]
-                p = j
-        lst[p] = lst[i]
-        lst[i] = m
+                m, p = lst[j], j
+        lst[p], lst[i] = lst[i], lst[p]
     return lst
 
-
+# O(n^2)
 def insertion_sort(_lst: list) -> list:
-    lst = _lst
-    ...
+    lst = _lst.copy()
+    N = len(lst)
+    for i in range(1, N):
+        for j in range(i, 0, -1):
+            if lst[j] < lst[j-1]:
+                lst[j], lst[j-1] = lst[j-1], lst[j]
+            else:
+                break
     return lst
 
-
+# O(n^2)
 def bubble_sort(_lst: list) -> list:
     lst = _lst.copy()
     ...
