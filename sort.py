c64c7c5d1e3b6dde454f4a00626b1e114f78c7a8
Julian <ziminjulian@gmail.com>
2025-08-06 14:52:55 +0300

    Created sort.py

---
+++ sort.py
@@ -0,0 +1,14 @@
+def selection_sort(lst: list) -> list:
+    N = len(lst)
+    for i in range(N-1):
+        m = lst[i]
+        p = i
+        for j in range(i+1, N):
+            if lst[j] < m:
+                m = lst[j]
+                p = j
+        lst[p] = lst[i]
+        lst[i] = m
+    return lst
+
+
